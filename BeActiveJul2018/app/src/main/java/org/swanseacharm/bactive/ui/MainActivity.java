ca79bf9b3f972ffcc8f1c190924c60fe9c8b1f95
Kfewell <kieran.fewell@gmail.com>
2018-06-29 16:58:43 +0100

    first GUI added
    Main gui added to program

--- BeActiveJul2018/app/src/main/java/org/swanseacharm/bactive/ui/MainActivity.java
+++ BeActiveJul2018/app/src/main/java/org/swanseacharm/bactive/ui/MainActivity.java
@@ -16,42 +16,18 @@ import org.swanseacharm.bactive.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private TextView mTextview;
     private Receiver receiver;
 
     private int mStepsToday;
     private long mTotSteps;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.constraint_layout);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
+        
     }
 
     @Override
